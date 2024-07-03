d91f36077b56827c201b8c40e6b406337f6c2feb
Erik <eait84344@gmail.com>
2024-07-03 16:30:02 +0400

    added category icons

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div className='menu__container'>
+      <div className="menu__content">
+        <div className="categories__container">
+          <div className="bx bx-home-alt"></div>
+          <div className='bx bx-search'></div>
+          <div className='bx bx-tv'></div>
+          <div className='bx bxs-camera-movie'></div>
+          <div className='bx bx-desktop'></div>
+          <div className='bx bx-hearts'></div>
+        </div>
+
+        <div className="settings">
+
+        </div>
+
+        <div className="menu__icon">
+
+        </div>
+      </div>
+    </div>
+  )
+}
