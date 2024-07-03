1dd264b93c2272fd3495acc23254df12947af914
Erik <eait84344@gmail.com>
2024-07-03 20:04:59 +0400

    added maps for rendering movies

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 export default function Category({ movieData, categoryData }) {
   return (
     <div className='category__container'>
-
+      {
+        categoryData.map((category) => {
+          return <h2 key={category.category_id}>{category.category_name}</h2>
+        })
+      }
+      <div className="category__content">
+        {
+          movieData.map((movie) => {
+            return <div></div>
+          })
+        }
+      </div>
     </div>
   )
 }
