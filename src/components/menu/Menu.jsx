00dc6d5e8af6cd9eadc7ec5d75907ccb04734492
Erik <eait84344@gmail.com>
2024-07-03 19:08:54 +0400

    changed some icons

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -17,9 +17,9 @@ export default function Menu() {
         </div>
 
         <div className="categories__container">
-          <div className='bx bxs-camera-movie'></div>
-          <div className='bx bx-desktop'></div>
-          <div className='bx bx-heart'></div>
+          <div className='bx bxs-user'></div>
+          <div className='bx bx-cog'></div>
+          <div className='bx bx-power-off'></div>
         </div>
 
         <div className="menu__icon">
