86d3bd864c114ae72a4994d2eb4229449f0376df
Erik <eait84344@gmail.com>
2024-07-03 19:52:33 +0400

    request start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+// hooks import
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://inter.natv.fm/player_api.php?username=rokuappdev&password=20928292684&action=get_vod_categories')
+      .then((resolve) => resolve.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
     <div className='Home__container'>
 
