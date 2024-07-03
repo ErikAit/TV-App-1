490be759c13b1a70dbb7cc0aea1f6137583a2024
Erik <eait84344@gmail.com>
2024-07-03 20:03:10 +0400

    start movies request

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,21 @@ import './Home-css/home.css'
 
 export default function Home() {
   const [categories, setCategories] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetch('https://inter.natv.fm/player_api.php?username=rokuappdev&password=20928292684&action=get_vod_categories')
       .then((resolve) => resolve.json())
       .then((data) => setCategories(data));
+
+    fetch('https://inter.natv.fm/player_api.php?username=rokuappdev&password=20928292684&action=get_vod_streams')
+      .then((resolve) => resolve.json())
+      .then((data) => setMovies(data));
   }, []);
 
   return (
     <div className='Home__container'>
-      <Category categoryData={categories} />
+      <Category categoryData={categories} movieData={movies} />
     </div>
   )
 }
