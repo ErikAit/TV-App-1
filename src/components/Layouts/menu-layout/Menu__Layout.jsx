139026e183ffbcf8d3065f8dbe94a6f97ec10d8b
Erik <eait84344@gmail.com>
2024-07-03 16:52:05 +0400

    Menu.jsx called in Menu__Layout.jsx

--- src/components/Layouts/menu-layout/Menu__Layout.jsx
+++ src/components/Layouts/menu-layout/Menu__Layout.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 // route import
 import { Outlet } from 'react-router-dom'
 
+// component import
+import Menu from '../../menu/Menu'
+
 export default function Menu__Layout() {
   return (
     <div className='menu_layout_container'>
+      <Menu />
       <Outlet />
     </div>
   )
