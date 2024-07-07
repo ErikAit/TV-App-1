c7edd08a995d8289f644c67148541aa7d5daeaa5
Erik <eait84344@gmail.com>
2024-07-07 21:54:09 +0400

    Card.jsx called again

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -7,11 +7,11 @@ export default function Category({ movieData, categoryData }) {
   return (
     <div className='category__container'>
       <div className="category__content">
-        {/* {
+        {
           movieData.map((movie) => {
             return <Card key={movie.stream_id} data={movie} />
           })
-        } */}
+        }
       </div>
     </div>
   )
