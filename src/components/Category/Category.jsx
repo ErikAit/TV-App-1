3477226be2adf083d6da443db64cd7852c2ee9f8
Erik <eait84344@gmail.com>
2024-07-07 22:01:26 +0400

    added css file for Category.jsx

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 // coponents imports
 import Card from '../Card/Card'
 
+// css import
+import './Category-css/Category.css'
+
 export default function Category({ movieData, categoryData }) {
   return (
     <div className='category__container'>
