5d83a04d91f21ec194a5f209538e7ead59d8a91d
Erik <eait84344@gmail.com>
2024-07-07 15:59:34 +0400

    Card.jsx is commented

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Card({ data }) {
   return (
     <div className='Card'>
-
+      <img src={data.stream_icon} alt="movie poster" />
     </div>
   )
 }
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
+
+// coponents imports
 import Card from '../Card/Card'
 
 export default function Category({ movieData, categoryData }) {
   return (
     <div className='category__container'>
-      {
-        categoryData.map((category) => {
-          return <h2 key={category.category_id}>{category.category_name}</h2>
-        })
-      }
       <div className="category__content">
-        {
+        {/* {
           movieData.map((movie) => {
-            return <Card data={movie} />
+            return <Card key={movie.stream_id} data={movie} />
           })
-        }
+        } */}
       </div>
     </div>
   )
