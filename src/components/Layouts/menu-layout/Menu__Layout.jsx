238d9da6669b1611e5c77c3e80c8d8885909b48c
Erik <eait84344@gmail.com>
2024-07-09 20:02:36 +0400

    connected css file

--- src/components/Layouts/menu-layout/Menu__Layout.jsx
+++ src/components/Layouts/menu-layout/Menu__Layout.jsx
@@ -6,6 +6,9 @@ import { Outlet } from 'react-router-dom'
 // component import
 import Menu from '../../menu/Menu'
 
+// css import
+import './menu-layout-styles/MenuLayout.css'
+
 export default function Menu__Layout() {
   return (
     <div className='menu_layout_container'>
