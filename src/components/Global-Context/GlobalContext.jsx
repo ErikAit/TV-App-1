56c12d97a8af2fb35fd143a459981e861da1fe49
Erik <eait84344@gmail.com>
2024-07-09 20:46:22 +0400

    added new argument "children"

--- src/components/Global-Context/GlobalContext.jsx
+++ src/components/Global-Context/GlobalContext.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function GlobalContext() {
+export default function GlobalContext({ children }) {
   return (
     <div className='context'>
-
+      {children}
     </div>
   )
 }
