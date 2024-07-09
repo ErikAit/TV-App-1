dcc04813e499a98dd05fa26bdd5ccb54a7507a6a
Erik <eait84344@gmail.com>
2024-07-09 20:32:38 +0400

    changed fetchs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-// hooks import
-import React, { useEffect, useState } from 'react'
-
-// category import
-import Category from '../components/Category/Category';
+import React, { useEffect, useState } from 'react';
 
 // css import
-import './Home-css/home.css'
+import './Home-css/home.css';
+
+// components import
+import Category from '../components/Category/Category';
 import HillightMovie from '../components/Hillight-Movie/HillightMovie';
+import Loading from '../components/Loading/Loading';
 
 export default function Home() {
   const [categories, setCategories] = useState([]);
@@ -14,20 +14,32 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetch('https://inter.natv.fm/player_api.php?username=rokuappdev&password=20928292684&action=get_vod_categories')
-      .then((resolve) => resolve.json())
-      .then((data) => setCategories(data));
+    const fetchData = async () => {
+      setLoading(true);
 
-    fetch('https://inter.natv.fm/player_api.php?username=rokuappdev&password=20928292684&action=get_vod_streams')
-      .then((resolve) => resolve.json())
-      .then((data) => setMovies(data));
+      try {
+        const categoryResponse = await fetch('https://inter.natv.fm/player_api.php?username=rokuappdev&password=20928292684&action=get_vod_categories');
+        const categoryData = await categoryResponse.json();
+        setCategories(categoryData);
+
+        const moviesResponse = await fetch('https://inter.natv.fm/player_api.php?username=rokuappdev&password=20928292684&action=get_vod_streams');
+        const moviesData = await moviesResponse.json();
+        setMovies(moviesData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
     <div className='Home__container'>
+      {loading && <Loading />}
       <HillightMovie data={movies} />
-
       <Category categoryData={categories} movieData={movies} />
     </div>
-  )
+  );
 }
