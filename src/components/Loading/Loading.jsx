0c9d91059f09ff37c507a198e8e6718f805bb9e3
Erik <eait84344@gmail.com>
2024-07-09 20:18:44 +0400

    added loading gif

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className='loading__container'>
-
+      <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif" alt="loading gif" />
     </div>
   )
 }
