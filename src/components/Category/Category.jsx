d185b378a2cf4760eb6de3949b843fae591007c9
Erik <eait84344@gmail.com>
2024-07-12 02:32:48 +0400

    removed arguments and get categories and movies from Global Context

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,24 +2,26 @@ import React, { useContext } from 'react'
 
 // coponents imports
 import Card from '../Card/Card'
-import { IndexContext } from '../Global-Context/GlobalContext';
+import { AllMoviesContext, IndexContext } from '../Global-Context/GlobalContext';
 
 // css import
 import './Category-css/Category.css'
 
-export default function Category({ movieData, categoryData }) {
+export default function Category() {
   const { selectedMovieIndex, getFocusedMovieIndex } = useContext(IndexContext);
+
+  const { categories, movies } = useContext(AllMoviesContext);
 
   return (
     <div className='category__container'>
       {
-        categoryData.map((category) => {
+        categories.map((category) => {
           return (
             <div key={category.category_id}>
               <h2>{category.category_name}</h2>
               <div className="category__content">
                 {
-                  movieData.map((movie, index) => {
+                  movies.map((movie, index) => {
                     if (movie.category_id === category.category_id) {
                       return <Card
                         key={movie.stream_id}
