f9361af5ee2c30ccefc062b630261a2f95084fed
Erik <eait84344@gmail.com>
2024-07-24 19:38:12 +0400

    added menu titles focus functional

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,86 +1,61 @@
-import React, { useContext } from 'react'
-
-// css import
-import './menu-styles/Menu.css'
-import { NavLink } from 'react-router-dom'
-import { useFocusStore } from '../../requests/requests'
+// CSS Import
+import './menu-styles/Menu.css';
+import { useFocusStore, useMenuTitle } from '../../requests/requests';
 
 export default function Menu() {
   const isFirstSelected = useFocusStore((state) => state.isFirstSelected);
+  const focusedTitle = useFocusStore((state) => state.focusedTitle);
+  const setFocusedTitle = useFocusStore((state) => state.setFocusedTitle);
+  const { menuTitles } = useMenuTitle();
 
   return (
-    <div className={`menu__container ${isFirstSelected === true ? 'active' : ''}`}>
+    <div className={`menu__container ${isFirstSelected ? 'active' : ''}`}>
       <div className='menu__content'>
         <div className="categories__container">
           <div className="bx bx-home-alt"></div>
-
           <div className='bx bx-search'></div>
-
           <div className='bx bx-tv'></div>
-
-
           <div className='bx bxs-camera-movie'></div>
-
-
           <div className='bx bx-desktop'></div>
-
           <div className='bx bx-heart'></div>
         </div>
 
         <div className="categories__container">
           <div className='bx bxs-user'></div>
-
           <div className='bx bx-cog'></div>
-
           <div className='bx bx-power-off'></div>
-        </div>
-
-        <div className="menu__icon">
-
         </div>
       </div>
 
       <div className="open__menu__container">
         <div className="open__menu__content">
           <div>
-            <div>
-              <span>lorem</span>
-            </div>
-            <div>
-              <span>lorem</span>
-            </div>
-            <div>
-              <span>lorem</span>
-            </div>
-            <div>
-              <span>lorem</span>
-            </div>
-            <div>
-              <span>lorem</span>
-            </div>
-            <div>
-              <span>lorem</span>
-            </div>
+            {menuTitles.slice(0, 6).map((menu, index) => (
+              <div key={menu.id}>
+                <span
+                  className={index === focusedTitle ? 'focused' : ''}
+                  onMouseEnter={() => setFocusedTitle(index)}
+                >
+                  {menu.title}
+                </span>
+              </div>
+            ))}
           </div>
 
           <div>
-            <div>
-              <span>lorem</span>
-            </div>
-            <div>
-              <span>lorem</span>
-            </div>
-            <div>
-              <span>lorem</span>
-            </div>
+            {menuTitles.slice(6).map((menu, index) => (
+              <div key={menu.id}>
+                <span
+                  className={index + 6 === focusedTitle ? 'focused' : ''}
+                  onMouseEnter={() => setFocusedTitle(index + 6)}
+                >
+                  {menu.title}
+                </span>
+              </div>
+            ))}
           </div>
-
-          <div>
-
-          </div>
-
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
--- src/requests/requests.js
+++ src/requests/requests.js
@@ -18,16 +18,33 @@ export const useMovieStore = create((set) => ({
   },
 }));
 
+export const useMenuTitle = create((set) => ({
+  menuTitles: [
+    { id: 1, title: 'lorem' },
+    { id: 2, title: 'lorem' },
+    { id: 3, title: 'lorem' },
+    { id: 4, title: 'lorem' },
+    { id: 5, title: 'lorem' },
+    { id: 6, title: 'lorem' },
+    { id: 7, title: 'lorem' },
+    { id: 8, title: 'lorem' },
+    { id: 9, title: 'lorem' },
+  ],
+}));
+
 export const useFocusStore = create((set, get) => ({
   selectedIndex: 0,
   focusedCategoryIndex: 0,
   isFirstSelected: false,
+  focusedTitle: 0,
   setSelectedIndex: (index) => set({ selectedIndex: index }),
   setFocusedCategoryIndex: (index) => set({ focusedCategoryIndex: index }),
+  setFocusedTitle: (index) => set({ focusedTitle: index }),
   handleKeyDown: (e) => {
-    const { selectedIndex, focusedCategoryIndex, isFirstSelected } = get();
+    const { selectedIndex, focusedCategoryIndex, isFirstSelected, focusedTitle } = get();
     const { categories } = useCategoryStore.getState();
     const { movies } = useMovieStore.getState();
+    const { menuTitles } = useMenuTitle.getState();
 
     const getMoviesByCategory = (categoryId) => {
       return movies.filter(movie => movie.category_id === categoryId);
@@ -41,21 +58,33 @@ export const useFocusStore = create((set, get) => ({
       if (e.key === 'ArrowLeft') {
         if (selectedIndex === 0) {
           set({ isFirstSelected: true });
+          set({ selectedIndex: -1 });
         } else {
           set({ selectedIndex: Math.max(selectedIndex - 1, 0) });
         }
       } else if (e.key === 'ArrowRight') {
         set({ selectedIndex: Math.min(selectedIndex + 1, focusedCategoryMovies.length - 1) });
+        if (isFirstSelected) {
+          set({ selectedIndex: 0 });
+        }
         set({ isFirstSelected: false });
-      } else if (e.key === 'ArrowUp' && isFirstSelected !== true) {
-        if (focusedCategoryIndex > 0) {
+      } else if (e.key === 'ArrowUp') {
+        if (isFirstSelected) {
+          if (focusedTitle > 0) {
+            set({ focusedTitle: focusedTitle - 1 });
+          }
+        } else if (focusedCategoryIndex > 0) {
           const newCategoryIndex = focusedCategoryIndex - 1;
           const newCategoryMovies = getMoviesByCategory(categories[newCategoryIndex].category_id);
           set({ focusedCategoryIndex: newCategoryIndex });
           set({ selectedIndex: Math.min(selectedIndex, newCategoryMovies.length - 1) });
         }
-      } else if (e.key === 'ArrowDown' && isFirstSelected !== true) {
-        if (focusedCategoryIndex < categories.length - 1) {
+      } else if (e.key === 'ArrowDown') {
+        if (isFirstSelected) {
+          if (focusedTitle < menuTitles.length - 1) {
+            set({ focusedTitle: focusedTitle + 1 });
+          }
+        } else if (focusedCategoryIndex < categories.length - 1) {
           const newCategoryIndex = focusedCategoryIndex + 1;
           const newCategoryMovies = getMoviesByCategory(categories[newCategoryIndex].category_id);
           set({ focusedCategoryIndex: newCategoryIndex });
@@ -63,5 +92,5 @@ export const useFocusStore = create((set, get) => ({
         }
       }
     }
-  }
+  },
 }));
