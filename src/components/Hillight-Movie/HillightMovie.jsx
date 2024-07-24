ff05d2e72205b68b611526af6dfcc9f14a7dd088
Erik <eait84344@gmail.com>
2024-07-24 20:11:35 +0400

    added hillight movie

--- src/components/Hillight-Movie/HillightMovie.jsx
+++ src/components/Hillight-Movie/HillightMovie.jsx
@@ -3,15 +3,14 @@ import React from 'react'
 // css import
 import './Hillight-Movie-styles/hillightMovie.css'
 
-export default function HillightMovie({ data }) {
+export default function HillightMovie() {
+
   return (
     <div className='hillight-movie__container'>
-      {
-        data.length !== 0 &&
-        <div style={{ backgroundImage: `url(https://images.iptv.rt.ru/images/c6u8v23ir4sslltu9m9g.jpg)` }} className="movie_image">
-          <h2>Невидимый гость (2016)</h2>
-        </div>
-      }
+      <h2 className='title'>Невидимый гость (2016)</h2>
+      <div style={{ backgroundImage: `url(https://images.iptv.rt.ru/images/c6u8v23ir4sslltu9m9g.jpg)` }} className="movie_image">
+        <div className="overlay"></div>
+      </div>
     </div>
   )
 }
