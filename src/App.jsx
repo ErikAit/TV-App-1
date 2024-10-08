e51b63e57b819d5bf9143ff1e1cd7518b75e0f86
Erik <eait84344@gmail.com>
2024-10-08 19:51:40 +0400

    Film Info component called in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Route, Routes } from 'react-router-dom'
 // component import
 import Menu__Layout from './components/Layouts/menu-layout/Menu__Layout'
 import Home from './pages/Home'
+import FilmInfoPage from './pages/Film-Info_Page/FilmInfoPage'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route element={<Menu__Layout />}>
           <Route path='/' element={<Home />} />
+          <Route path='/info' element={<FilmInfoPage />} />
         </Route>
       </Routes>
     </div>
---
+++ src/pages/Film-Info_Page/FilmInfoPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FilmInfoPage() {
+  return (
+    <div className="FilmInfoPages">
+
+    </div>
+  )
+}
